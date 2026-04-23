86079e4521eb01a39442e3f489bc7ec5564c8713
Rebeccas <rebeccas@mysanteplus.com>
2026-04-23 07:33:39 +0200

    Update supabaseClient.js

--- js/core/supabaseClient.js
+++ js/core/supabaseClient.js
@@ -4,8 +4,8 @@
 
 const { createClient } = window.supabase;
 
-const supabaseUrl = "https://tagqwwfbpfzluahboczh.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhZ3F3d2ZicGZ6bHVhaGJvY3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MDYxMDYsImV4cCI6MjA5MDM4MjEwNn0.I0HqBYPTrxPOg41sEWm_hU7YY3f9ZXCekUX5NlgIBWw";
+const supabaseUrl = "https://bcliieqhymeubmsdkqyn.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbGlpZXFoeW1ldWJtc2RrcXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MTY1NDksImV4cCI6MjA5MjI5MjU0OX0.wohWAn4emeWqZicjYv7jDq8xzZFNVZlEhZRWr1xEog8";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
